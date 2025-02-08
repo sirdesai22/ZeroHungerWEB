5c32143622f163fd1da475cdcaabdfd3da8ac5b9
sirdesai22 <sirdesai.work@gmail.com>
2025-02-08 10:50:39 +0530

    basic api setup

---
+++ src/app/api/leaderboard/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest, NextResponse } from "next/server"
+
+// GET handler
+export async function GET() {
+    return NextResponse.json({ message: "Hello World!" }, { status: 200 })
+}
