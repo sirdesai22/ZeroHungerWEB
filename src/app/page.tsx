5e30a3a98c8cc4c7d12c35c421d3d3fd4bb19a5a
sirdesai22 <sirdesai.work@gmail.com>
2025-02-08 10:36:53 +0530

    basic api setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,65 @@
-import Image from "next/image";
+"use client"
+
+import Script from "next/script";
+import Razorpay from "razorpay";
+import { useState } from "react";
+
+declare global {
+  interface Window{
+    Razorpay: any;
+  }
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const AMOUNT = 100;
+  const [isProcessing, setIsProcessing] = useState(false);
+  const handlePayment = async () => {
+    setIsProcessing(true);
+    try {
+      const response  = await fetch("/api/payment", {method: "POST"})
+      const data = await response.json();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      const options = {
+        key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+        amount: AMOUNT *100,
+        currency: "INR",
+        name: "Prathamesh Sirdesai",
+        description: "Test Transaction",
+        order_id: data.orderId,
+        handler: function(response:any){
+          console.log("Payment successful", response)
+        },
+        prefill: {
+          name: "John Doe",
+          email: "johndoe@gail.com",
+          contact: "1234567890",
+        },
+        theme: {
+          color: "#3399cc",
+        },
+      }
+
+      const rzp1 = new window.Razorpay(options);
+      rzp1.open();
+
+    } catch (error) {
+      console.error("Payment failed", error);
+    } finally {
+      setIsProcessing(false);
+    }
+
+  }
+
+  return (
+    <div className="min-h-screen w-full flex flex-col justify-center items-center gap-3">
+      <Script src="https://checkout.razorpay.com/v1/checkout.js" />
+      <h1 className="text-4xl font-bold">Payment Page</h1>
+      <button 
+      disabled={isProcessing}
+      onClick={handlePayment}
+      className="text-white bg-blue-500 font-semibold text-3xl px-4 py-2 rounded-md">
+        {isProcessing? "Processing..." : "Pay Now"}
+      </button>
     </div>
   );
 }
