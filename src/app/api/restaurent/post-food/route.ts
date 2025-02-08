8f564217e5d3d43567b235697c4b549441b3f675
sirdesai22 <sirdesai.work@gmail.com>
2025-02-08 20:32:08 +0530

    store food image in bucket

--- src/app/api/restaurent/post-food/route.ts
+++ src/app/api/restaurent/post-food/route.ts
@@ -9,7 +9,7 @@ export async function GET() {
 export async function POST(req: NextRequest) {
     try {
         const body = await req.json(); // Parse request body
-        const { user_id, status, name, quantity, veg } = body;
+        const { user_id, status, name, quantity, veg, image } = body;
 
         // Validate input
         if (!user_id || typeof status !== "boolean" || typeof quantity !== "number" || typeof veg !== "boolean") {
@@ -20,17 +20,30 @@ export async function POST(req: NextRequest) {
         const { data: food, error } = await supabase
             .from("food")
             .insert({
-                    user_id: user_id,
-                    status: status,
-                    name: name,
-                    quantity: quantity,
-                    veg: veg
-                },
-            );
+                user_id: user_id,
+                status: status,
+                name: name,
+                quantity: quantity,
+                veg: veg
+            }).single();
 
         if (error) {
             console.error("Error fetching food items:", error);
             return NextResponse.json({ error: "Error fetching food items" }, { status: 500 });
+        }
+
+        const avatarFile = image;
+        const { data, error: storageError } = await supabase
+            .storage
+            .from('food-images')
+            .upload(`${food?.id}.png`, image, {
+                cacheControl: '3600',
+                upsert: false
+            })
+
+        if (storageError) {
+            console.error("Error uploading avatar:", storageError);
+            return NextResponse.json({ error: "Error uploading avatar" }, { status: 500 });
         }
 
         return NextResponse.json({ success: true, data: food }, { status: 200 });
